f4c123d542679667b17e2beffc95c6371872f1ba
Rohit Nepali <rohitnepali75@gmail.com>
2025-03-24 07:32:45 +0545

    middleware and route

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { authenticate } from '../middleware/authMiddleware.js';
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json({ message: 'User routes' });
 });
 
+router.get('/profile', authenticate, (req, res) => {
+    res.json({ user: req.user });
+});
 
 export default router;
